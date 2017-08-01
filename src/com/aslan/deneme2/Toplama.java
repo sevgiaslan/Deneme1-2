f480252a9329c8698d01873775fa32de83557bed
Sevgi Aslan <aslannsevgi@gmail.com>
2017-08-01 11:19:53 +0300

    toplama classı ekledim

---
+++ src/com/aslan/deneme2/Toplama.java
@@ -0,0 +1,20 @@
+package com.aslan.deneme2;
+import java.util.*;
+
+public class Toplama {
+
+	public static void main(String[] args) {
+		int sayi1,sayi2,toplam;//Deðiþkenlerimizi tanýmlýyoruz.
+	    Scanner giris=new Scanner(System.in);//
+	    
+	    System.out.print("Bir sayý girin:");
+	    sayi1=giris.nextInt();
+	     System.out.print("Bir sayý girin:");
+	    sayi2=giris.nextInt();
+	    toplam=sayi1+sayi2;
+	       System.out.print("Sayýlarýn Toplamý:"+toplam);// sonucu ekrana yazdýrýyoruz.
+	    }
+
+	}
+
+
