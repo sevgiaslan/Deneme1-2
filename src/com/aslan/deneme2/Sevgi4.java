c892067878fd035af8d23251569e75dbfe927603
Sevgi Aslan <aslannsevgi@gmail.com>
2017-08-01 11:03:51 +0300

    yeni class ekledim

---
+++ src/com/aslan/deneme2/Sevgi4.java
@@ -0,0 +1,11 @@
+package com.aslan.deneme2;
+
+public class Sevgi4 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Deðiþiklik yapma alýþtýrmasý yapýyorum.");
+		
+}
+
+}
