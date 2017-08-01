df3ecbfa09c29c7335a039576b2e96cc5e2346ed
Sevgi Aslan <aslannsevgi@gmail.com>
2017-08-01 10:45:25 +0300

    ilk yorum

---
+++ src/com/aslan/deneme2/Sevgi3.java
@@ -0,0 +1,10 @@
+package com.aslan.deneme2;
+
+public class Sevgi3 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Eclipse çalýþmasýný github'a gönderme.");
+		
+}
+}
